5324a0c40907bc7989e75376724f26b609ffefe0
Robert Schadek <rschadek@symmetryinvestments.com>
2019-08-22 17:10:15 +0100

    some more testing

--- source/juliad/evalstringtest.d
+++ source/juliad/evalstringtest.d
@@ -2,6 +2,12 @@ module juliad.evalstringtest;
 import juliad;
 
 unittest {
-	auto r = jlEvalString("4 + 5");
+	import std.stdio;
+
+	_jl_value_t* ret = jlEvalString("4 + 5");
+	if(jl_typeis(ret, jl_float64_type)) {
+    	double retUnboxed = jl_unbox_float64(ret);
+		writeln(retUnboxed);
+	}
 }
 
