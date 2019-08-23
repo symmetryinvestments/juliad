eed4485ded1658175c3116ad2e1ad3c3ebf17399
Robert burner Schadek <rburners@gmail.com>
2019-08-23 09:30:59 +0100

    julia.h is not nice

--- source/juliad/evalstringtest.d
+++ source/juliad/evalstringtest.d
@@ -1,13 +1,43 @@
 module juliad.evalstringtest;
+
+import std.format;
+
 import juliad;
 
 unittest {
 	import std.stdio;
 
-	_jl_value_t* ret = jlEvalString("4 + 5");
+	jl_value_t* ret = jlEvalString("sqrt(2.0)");
+
 	if(jl_typeis(ret, jl_float64_type)) {
+    	double retUnboxed = jl_unbox_float64(ret);
+		writeln(retUnboxed);
+	} else if(jl_typeis(ret, jl_int32_type)) {
     	double retUnboxed = jl_unbox_float64(ret);
 		writeln(retUnboxed);
 	}
 }
 
+unittest {
+	import std.stdio;
+
+	jl_value_t* ret = jlEvalString("1 + 2");
+	static foreach(t; genJlTypes) {{
+		enum g = format(`bool i = jl_is_%s(ret);`, t);
+		mixin(g);
+		writeln(t, " ", i);
+	}}
+	if(jl_is_int32(ret)) {
+    	double retUnboxed = jl_unbox_int32(ret);
+		writeln(retUnboxed);
+	} else if(jl_is_int64(ret)) {
+    	double retUnboxed = jl_unbox_int64(ret);
+		writeln(retUnboxed);
+	} else if(jl_typeis(ret, jl_float64_type)) {
+    	double retUnboxed = jl_unbox_float64(ret);
+		writeln(retUnboxed);
+	} else if(jl_typeis(ret, jl_float32_type)) {
+    	double retUnboxed = jl_unbox_float32(ret);
+		writeln(retUnboxed);
+	}
+}
